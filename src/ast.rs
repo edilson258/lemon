a33bb29946f14798c017d1612a1c7f476e992c14
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-11-01 21:30:42 +0200

    *

--- src/ast.rs
+++ src/ast.rs
@@ -1,27 +1,28 @@
-use crate::utils::range::Range;
+#![allow(dead_code)]
+use crate::{tokens::TokenType, utils::range::Range};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct Program {
-  pub stmts: Vec<Stements>,
+  pub stmts: Vec<Stmts>,
 }
 
 // ------- statements -------
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
-pub enum Stements {
+pub enum Stmts {
   Let(LetStmt),
-  Expr(Expression),
+  Expr(Expr),
   Fn(FunctionStmt),
   Block(BlockStmt),
 }
 
-impl Stements {
+impl Stmts {
   pub fn get_range(&self) -> &Range {
     match self {
-      Stements::Let(let_stmt) => let_stmt.get_range(),
-      Stements::Expr(expr) => expr.get_range(),
-      Stements::Fn(function_stmt) => function_stmt.get_range(),
-      Stements::Block(block_stmt) => block_stmt.get_range(),
+      Stmts::Let(let_stmt) => let_stmt.get_range(),
+      Stmts::Expr(expr) => expr.get_range(),
+      Stmts::Fn(function_stmt) => function_stmt.get_range(),
+      Stmts::Block(block_stmt) => block_stmt.get_range(),
     }
   }
 }
@@ -30,11 +31,11 @@ impl Stements {
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct LetStmt {
   pub pat: Pat,
-  pub expr: Expression,
+  pub expr: Expr,
   pub range: Range,
 }
 impl LetStmt {
-  pub fn create(pat: Pat, expr: Expression, range: Range) -> Self {
+  pub fn create(pat: Pat, expr: Expr, range: Range) -> Self {
     Self { pat, expr, range }
   }
   pub fn get_range(&self) -> &Range {
@@ -48,13 +49,13 @@ pub struct FunctionStmt {
   pub name: Identifier,
   pub pats: Vec<Pat>,
   pub ty: Option<String>,
-  pub body: BlockStmt,
+  pub body: Box<Stmts>,
   pub range: Range,
 }
 
 impl FunctionStmt {
-  pub fn create(name: Identifier, pats: Vec<Pat>, body: BlockStmt, ty: Option<String>, range: Range) -> Self {
-    Self { name, pats, body, ty, range }
+  pub fn create(name: Identifier, pats: Vec<Pat>, body: Stmts, ty: Option<String>, range: Range) -> Self {
+    Self { name, pats, body: Box::new(body), ty, range }
   }
   pub fn get_range(&self) -> &Range {
     &self.range
@@ -63,12 +64,12 @@ impl FunctionStmt {
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct BlockStmt {
-  pub stmts: Vec<Stements>,
+  pub stmts: Vec<Stmts>,
   pub range: Range,
 }
 
 impl BlockStmt {
-  pub fn create(stmts: Vec<Stements>, range: Range) -> Self {
+  pub fn create(stmts: Vec<Stmts>, range: Range) -> Self {
     Self { stmts, range }
   }
   pub fn get_range(&self) -> &Range {
@@ -109,13 +110,15 @@ impl Pat {
 
 // ------- expressions -------
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
-pub enum Expression {
+pub enum Expr {
   Group(GroupExpr),
+  Fn(FnExpr),
   Binary(BinaryExpr),
+  Pipe(PipeExpr),
   Unary(UnaryExpr),
   Call(CallExpr),
   Match(MatchExpr),
-  Index(IndexExpr),
+  Idx(IdxExpr),
   Member(MemberExpr),
   If(IfExpr),
   Return(ReturnExpr),
@@ -123,28 +126,42 @@ pub enum Expression {
   Literal(Literal),
 }
 
-impl Expression {
+impl Expr {
   pub fn get_range(&self) -> &Range {
     match self {
-      Expression::Group(group) => group.get_range(),
-      Expression::Binary(binary) => binary.get_range(),
-      Expression::Unary(unary) => unary.get_range(),
-      Expression::Call(call) => call.get_range(),
-      Expression::Match(match_expr) => match_expr.get_range(),
-      Expression::Index(index) => index.get_range(),
-      Expression::Member(member) => member.get_range(),
-      Expression::If(if_expr) => if_expr.get_range(),
-      Expression::Return(return_expr) => return_expr.get_range(),
-      Expression::Ident(ident) => ident.get_range(),
-      Expression::Literal(literal) => literal.get_range(),
+      Expr::Fn(fn_expr) => fn_expr.get_range(),
+      Expr::Group(group) => group.get_range(),
+      Expr::Binary(binary) => binary.get_range(),
+      Expr::Pipe(pipe) => pipe.get_range(),
+      Expr::Unary(unary) => unary.get_range(),
+      Expr::Call(call) => call.get_range(),
+      Expr::Match(match_expr) => match_expr.get_range(),
+      Expr::Idx(index) => index.get_range(),
+      Expr::Member(member) => member.get_range(),
+      Expr::If(if_expr) => if_expr.get_range(),
+      Expr::Return(return_expr) => return_expr.get_range(),
+      Expr::Ident(ident) => ident.get_range(),
+      Expr::Literal(literal) => literal.get_range(),
     }
+  }
+}
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct FnExpr {
+  pub pats: Vec<Pat>,
+  pub body: Box<Stmts>,
+  pub range: Range,
+}
+
+impl FnExpr {
+  pub fn get_range(&self) -> &Range {
+    &self.range
   }
 }
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct GroupExpr {
   pub range: Range,
-  pub expr: Box<Expression>,
+  pub expr: Box<Expr>,
 }
 
 impl GroupExpr {
@@ -154,10 +171,26 @@ impl GroupExpr {
 }
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
-pub struct BinaryExpr {
-  pub left: Box<Expression>,
-  pub right: Box<Expression>,
+pub struct PipeExpr {
+  pub left: Box<Expr>,
+  pub right: Box<Expr>,
+  pub range_op: Range,
   pub range: Range,
+}
+
+impl PipeExpr {
+  pub fn get_range(&self) -> &Range {
+    &self.range
+  }
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct BinaryExpr {
+  pub left: Box<Expr>,
+  pub right: Box<Expr>,
+  pub operator: Operator,
+  pub range: Range,
+  pub range_op: Range,
 }
 
 impl BinaryExpr {
@@ -168,9 +201,10 @@ impl BinaryExpr {
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct UnaryExpr {
-  pub operand: Box<Expression>,
+  pub operand: Box<Expr>,
   pub operator: Operator,
   pub range: Range,
+  pub range_op: Range,
 }
 
 impl UnaryExpr {
@@ -181,8 +215,8 @@ impl UnaryExpr {
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct CallExpr {
-  pub callee: Box<Expression>,
-  pub args: Vec<Expression>,
+  pub callee: Box<Expr>,
+  pub args: Vec<Expr>,
   pub range: Range,
 }
 
@@ -195,7 +229,7 @@ impl CallExpr {
 // match <expr> { <arms> }
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct MatchExpr {
-  pub expr: Box<Expression>,
+  pub expr: Box<Expr>,
   pub arms: Vec<Arm>,
   pub range: Range,
 }
@@ -209,8 +243,8 @@ impl MatchExpr {
 // { <guard> } => { <stmts> }
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct Arm {
-  pub guard: Option<Box<Expression>>,
-  pub body: Vec<Stements>,
+  pub guard: Box<Expr>,
+  pub body: Stmts,
   pub range: Range,
 }
 
@@ -221,13 +255,13 @@ impl Arm {
 }
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
-pub struct IndexExpr {
-  pub object: Box<Expression>,
-  pub index: Box<Expression>,
+pub struct IdxExpr {
+  pub object: Box<Expr>,
+  pub index: Box<Expr>,
   pub range: Range,
 }
 
-impl IndexExpr {
+impl IdxExpr {
   pub fn get_range(&self) -> &Range {
     &self.range
   }
@@ -235,8 +269,8 @@ impl IndexExpr {
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct MemberExpr {
-  pub object: Box<Expression>,
-  pub property: String,
+  pub object: Box<Expr>,
+  pub property: Box<Expr>,
   pub range: Range,
 }
 
@@ -248,9 +282,9 @@ impl MemberExpr {
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct IfExpr {
-  pub condition: Box<Expression>,
-  pub consequent: Vec<Stements>,
-  pub alternate: Option<Vec<Stements>>,
+  pub condition: Box<Expr>,
+  pub consequent: Vec<Stmts>,
+  pub alternate: Option<Vec<Stmts>>,
   pub range: Range,
 }
 
@@ -262,7 +296,7 @@ impl IfExpr {
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct ReturnExpr {
-  pub value: Option<Box<Expression>>,
+  pub value: Option<Box<Expr>>,
   pub range: Range,
 }
 
@@ -339,34 +373,53 @@ impl NullLiteral {
 }
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
-pub struct Operator {
-  pub range: Range,
-  pub kind: OperatorEnum,
+pub enum Operator {
+  ADD,  // +
+  SUB,  // -
+  MUL,  // *
+  DIV,  // /
+  REM,  // %
+  EQ,   // ==
+  NEQ,  // !=
+  LT,   // <
+  GT,   // >
+  AND,  // &&
+  OR,   // ||
+  XOR,  // ^
+  SHL,  // <<
+  SHR,  // >>
+  POW,  // **
+  LE,   // <=
+  GE,   // >=
+  NOT,  // !
+  PIPE, // |>
 }
 
 impl Operator {
-  pub fn get_range(&self) -> &Range {
-    &self.range
+  pub fn from_token(token: &TokenType) -> Option<Self> {
+    match token {
+      TokenType::Plus => Some(Self::ADD),
+      TokenType::Minus => Some(Self::SUB),
+      TokenType::Star => Some(Self::MUL),
+      TokenType::Slash => Some(Self::DIV),
+      TokenType::Eq => Some(Self::EQ),
+      TokenType::NotEq => Some(Self::NEQ),
+      TokenType::LessEq => Some(Self::LE),
+      TokenType::GreaterEq => Some(Self::GE),
+      TokenType::Less => Some(Self::LT),
+      TokenType::Greater => Some(Self::GT),
+      TokenType::And => Some(Self::AND),
+      TokenType::Or => Some(Self::OR),
+      // TokenType::Dot => Some(Self::DOT),
+      // TokenType::Extract => Some(Self::EXTRACT),
+      TokenType::Pipe => Some(Self::PIPE),
+      // TokenType::Assign => Some(Self::ASSIGN),
+      TokenType::PlusEq => Some(Self::ADD),
+      TokenType::MinusEq => Some(Self::SUB),
+      TokenType::StarEq => Some(Self::MUL),
+      TokenType::SlashEq => Some(Self::DIV),
+      TokenType::Bang => Some(Self::NOT),
+      _ => None,
+    }
   }
-}
-
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
-pub enum OperatorEnum {
-  ADD, // +
-  SUB, // -
-  MUL, // *
-  DIV, // /
-  REM, // %
-  EQ,  // ==
-  NEQ, // !=
-  LT,  // <
-  GT,  // >
-  AND, // &&
-  OR,  // ||
-  XOR, // ^
-  SHL, // <<
-  SHR, // >>
-  POW, // **
-  LE,  // <=
-  GE,  // >=
 }
