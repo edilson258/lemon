4be0063c22b34a90614eb53085e887343df65865
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-12-01 23:11:21 +0200

    *

---
+++ src/runtime/mod.rs
@@ -0,0 +1,12 @@
+#![allow(dead_code)]
+pub struct Runtime {}
+
+impl Runtime {
+  pub fn init() {
+    todo!("runtime initializing");
+  }
+
+  pub fn shutdown(self) {
+    todo!("runtime shutting down");
+  }
+}
