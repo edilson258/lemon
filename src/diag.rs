cc95e81fdeac671676c68f79962771e1365f5adc
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-12-01 23:11:20 +0200

    *

--- src/diag.rs
+++ src/diag.rs
@@ -1,12 +1,13 @@
 #![allow(dead_code)]
 
-use std::{mem::take, path::PathBuf};
+use std::path::PathBuf;
 
 use serde::{Deserialize, Serialize};
 
 use crate::{
   range::Range,
   report::{report_err, report_info, report_warn},
+  source::Source,
 };
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
@@ -16,77 +17,63 @@ pub enum Severity {
   Info,
 }
 
-impl Severity {
-  pub fn to_string(&self) -> &str {
-    match self {
-      Severity::Err => "error",
-      Severity::Warn => "warning",
-      Severity::Info => "info",
-    }
-  }
-}
-
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct Diag {
   pub severity: Severity,
   pub message: String,
   pub range: Range,
-  pub path: PathBuf,
 }
 
 impl Diag {
-  pub fn new(severity: Severity, message: String, range: Range, path: PathBuf) -> Self {
-    Self { severity, message, range, path }
+  pub fn new(severity: Severity, message: String, range: Range) -> Self {
+    Self { severity, message, range }
   }
 
-  pub fn create_err(message: String, range: Range, path: PathBuf) -> Self {
-    Self::new(Severity::Err, message, range, path)
+  pub fn error(message: impl Into<String>, range: Range) -> Self {
+    Self::new(Severity::Err, message.into(), range)
   }
 
-  pub fn create_warn(message: String, range: Range, path: PathBuf) -> Self {
-    Self::new(Severity::Warn, message, range, path)
+  pub fn warning(message: impl Into<String>, range: Range) -> Self {
+    Self::new(Severity::Warn, message.into(), range)
   }
 
-  pub fn create_info(message: String, range: Range, path: PathBuf) -> Self {
-    Self::new(Severity::Info, message, range, path)
+  pub fn info(message: impl Into<String>, range: Range) -> Self {
+    Self::new(Severity::Info, message.into(), range)
   }
 
-  pub fn get_range(&self) -> &Range {
+  pub fn range(&self) -> &Range {
     &self.range
   }
 
-  pub fn report(&self) {
+  pub fn report(&self, path: &PathBuf) {
     match self.severity {
-      Severity::Err => report_err(&self),
-      Severity::Warn => report_warn(&self),
-      Severity::Info => report_info(&self),
+      Severity::Err => report_err(self, path),
+      Severity::Warn => report_warn(self, path),
+      Severity::Info => report_info(self, path),
     }
   }
-}
 
-pub struct DiagGroup {
-  pub diags: Vec<Diag>,
-}
-
-impl DiagGroup {
-  pub fn new() -> Self {
-    Self { diags: Vec::new() }
+  pub fn report_wrap(&self, path: &PathBuf) -> ! {
+    self.report(path);
+    std::process::exit(1);
   }
+}
 
+pub struct DiagGroup<'a> {
+  pub diags: Vec<Diag>,
+  pub source: &'a Source,
+}
+
+impl<'a> DiagGroup<'a> {
+  pub fn new(source: &'a Source) -> Self {
+    Self { diags: Vec::new(), source }
+  }
   pub fn add(&mut self, diag: Diag) {
     self.diags.push(diag);
   }
-
-  pub fn report(&self) {
-    //
-  }
-
-  pub fn ok(&mut self) -> Result<(), Self> {
-    if self.diags.is_empty() {
-      Ok(())
-    } else {
-      // report here?
-      Err(DiagGroup { diags: take(&mut self.diags) })
+  pub fn report(&self, path: &PathBuf) {
+    for diag in &self.diags {
+      diag.report(path);
     }
   }
 }
