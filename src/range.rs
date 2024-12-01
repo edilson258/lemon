c8f6046fd84edd4216af65a5319368bc142e61f8
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-12-01 23:11:20 +0200

    *

--- src/range.rs
+++ src/range.rs
@@ -1,9 +1,11 @@
 #![allow(dead_code)]
-
+use logos::Span;
 use serde::{Deserialize, Serialize};
+use std::cmp::{max, min};
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize, Eq)]
 pub struct Range {
+  // set as u32?
   pub start: usize,
   pub end: usize,
 }
@@ -12,12 +14,23 @@ impl Range {
   pub fn new(start: usize, end: usize) -> Range {
     Range { start, end }
   }
+
   pub fn merge(&mut self, range: &Range) {
-    if range.start < self.start {
-      self.start = range.start;
-    }
-    if range.end > self.end {
-      self.end = range.end;
-    }
+    self.start = min(self.start, range.start);
+    self.end = max(self.end, range.end);
   }
+
+  pub fn merged_with(&self, range: &Range) -> Range {
+    Range::new(min(self.start, range.start), max(self.end, range.end))
+  }
+
+  pub fn from_span(span: Span) -> Range {
+    Range::new(span.start, span.end)
+  }
+}
+
+// trait to acess range
+
+pub trait TraitRange {
+  fn range(&self) -> Range;
 }
