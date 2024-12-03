203d75d75c49e30438c25e03078095ba37dc576e
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-12-04 01:56:10 +0200

    *

--- src/checker/check_stmt.rs
+++ src/checker/check_stmt.rs
@@ -1,4 +1,4 @@
-use crate::{ast, range::TraitRange};
+use crate::ast::{self};
 
 use super::{diags::errs::TypeErr, types::Type, Checker, CheckerResult};
 
@@ -11,32 +11,34 @@ impl<'a> Checker<'a> {
       ast::Stmt::Block(block) => self.check_block_stmt(block),
     }
   }
-  pub fn check_fn_stmt(&mut self, fn_stmt: &ast::FnStmt) -> CheckerResult<Type> {
-    todo!()
-  }
-  pub fn check_let_stmt(&mut self, let_stmt: &ast::LetStmt) -> CheckerResult<Type> {
-    let value_type = match self.check_expr(&let_stmt.expr)? {
-      Some(value) => value,
-      None => return Ok(None),
-    };
 
-    if let Some(binding) = self.check_binding(&let_stmt.name)? {
-      if !binding.eq(&value_type) {
-        let range = let_stmt.name.ident.range();
-        let diag = TypeErr::Mismatched(binding.to_string(), value_type.to_string(), range);
-        return Err(diag.into());
+  pub fn check_let_stmt(&mut self, let_stmt: &ast::LetStmt) -> CheckerResult<Type> {
+    let value_type = self.check_expr(&let_stmt.expr)?.unwrap(); // we throw error in parser
+    match self.check_binding(&let_stmt.name)? {
+      Some(binding) if !binding.eq(&value_type) && !value_type.fits_in(&binding) => {
+        let range = let_stmt.expr.range();
+        // if are the sema set...  handle range out err...
+        if binding.same_set(&value_type) {
+          let diag = TypeErr::OutOfRange(&value_type, &binding, range);
+          return Err(diag.into());
+        }
+
+        let diag = TypeErr::Mismatched(&binding, &value_type, range);
+        Err(diag.into())
+      }
+      _ => {
+        self.ctx.add_value(let_stmt.get_name().to_owned(), value_type);
+        Ok(None)
       }
     }
-
-    self.ctx.add_value(let_stmt.get_name().to_owned(), value_type);
-    Ok(None)
   }
 
   pub fn check_block_stmt(&mut self, block: &ast::BlockStmt) -> CheckerResult<Type> {
-    let mut rt = None;
-    for stmt in &block.stmts {
-      rt = self.check_stmt(&stmt)?;
-    }
-    Ok(rt)
+    block.stmts.split_last().map_or(Ok(None), |(last, rest)| {
+      for stmt in rest {
+        self.check_stmt(stmt)?;
+      }
+      self.check_stmt(last)
+    })
   }
 }
