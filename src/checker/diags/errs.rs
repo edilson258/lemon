5586c11ba7fd0345f260c7003a0c29b9b6fdced2
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-12-04 01:56:10 +0200

    *

--- src/checker/diags/errs.rs
+++ src/checker/diags/errs.rs
@@ -1,29 +1,49 @@
 use crate::{
   ast::Operator,
+  checker::types::Type,
   diag::{self, Severity},
   range::Range,
 };
 
 #[derive(Debug, Clone)]
-pub enum TypeErr {
-  Mismatched(String, String, Range),
-  UnsupportedOperator(String, String, Operator, Range),
-  OutRange(String, String, String, String, Range),
-  ExpectedValue(String, Range),
-  NotInScope(String, Range),
-  NotAFunction(String, Range),
-  NotAnExpression(String, Range),
+pub enum TypeErr<'a> {
+  Mismatched(&'a Type, &'a Type, Range),
+  NotFound(&'a str, Range),
+  Unsupported(Range),
+  OutOfRange(&'a Type, &'a Type, Range),
+  NotSupported(&'a Type, &'a Type, &'a Operator, Range),
+  ExpectedValue(&'a Type, Range),
+  NoExpectedValue(&'a Type, Range),
+  NotInScope(&'a Type, Range),
+  NotAFunction(&'a Type, Range),
+  NotAnExpression(&'a Type, Range),
 }
 
-impl From<TypeErr> for diag::Diag {
+impl<'a> From<TypeErr<'a>> for diag::Diag {
   fn from(err: TypeErr) -> Self {
     match err {
       TypeErr::Mismatched(expected, actual, range) => {
-        let text = format!("expected '{}' but found '{}'", expected, actual);
+        let text = format!("expected '{}', found '{}'", expected, actual);
+        diag::Diag::new(Severity::Err, text, range)
+      }
+      TypeErr::NoExpectedValue(expected, range) => {
+        let text = format!("no expected value, found '{}'", expected);
+        diag::Diag::new(Severity::Err, text, range)
+      }
+      TypeErr::NotFound(name, range) => {
+        let text = format!("not found '{}' in scope", name);
+        diag::Diag::new(Severity::Err, text, range)
+      }
+      TypeErr::Unsupported(range) => {
+        let text = "unsupported value".to_owned();
+        diag::Diag::new(Severity::Err, text, range)
+      }
+      TypeErr::OutOfRange(value, expected, range) => {
+        let text = format!("'{}' is out of range, must be '{}'", value, expected);
         diag::Diag::new(Severity::Err, text, range)
       }
       TypeErr::ExpectedValue(name, range) => {
-        let text = format!("expected value but found '{}'", name);
+        let text = format!("expected '{}', found nothing", name);
         diag::Diag::new(Severity::Err, text, range)
       }
       TypeErr::NotInScope(name, range) => {
@@ -38,11 +58,11 @@ impl From<TypeErr> for diag::Diag {
         let text = format!("not an expression '{}'", name);
         diag::Diag::new(Severity::Err, text, range)
       }
-      TypeErr::UnsupportedOperator(lf, rt, operator, range) => {
+      TypeErr::NotSupported(lf, rt, operator, range) => {
         let text = match operator {
           Operator::ADD => format!("cannot add '{}' and '{}'", lf, rt),
           Operator::SUB => format!("cannot subtract '{}' from '{}'", lf, rt),
-          Operator::MUL => format!("cannot multiply '{}' and '{}'", lf, rt),
+          Operator::MUL => format!("cannot multiply '{}' by '{}'", lf, rt),
           Operator::DIV => format!("cannot divide '{}' by '{}'", lf, rt),
           Operator::MOD => format!("cannot take modulus of '{}' and '{}'", lf, rt),
           Operator::EQ => format!("cannot compare '{}' and '{}'", lf, rt),
@@ -56,11 +76,6 @@ impl From<TypeErr> for diag::Diag {
           Operator::RANGE => format!("cannot concatenate '{}' and '{}'", lf, rt),
           _ => todo!(),
         };
-        diag::Diag::new(Severity::Err, text, range)
-      }
-      // value `300` out of range for `u8` (0..=255)
-      TypeErr::OutRange(value, tyy, start, end, range) => {
-        let text = format!("value '{}' out of range for '{}' ({}..={})", value, tyy, start, end);
         diag::Diag::new(Severity::Err, text, range)
       }
     }
