8ae8388d29b764779a6dbd07b600b5da50b2bf48
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-12-05 00:57:28 +0200

    *

--- src/checker/check_fn.rs
+++ src/checker/check_fn.rs
@@ -19,7 +19,7 @@ impl<'a> Checker<'a> {
     }
 
     let ret_type = match &fn_stmt.ret_type {
-      Some(ret) => Some(Box::new(self.check_type(ret)?.unwrap())),
+      Some(ret) => Some(Box::new(self.check_type(ret)?.unwrap())), // we can unwrap here because we
       None => None,
     };
 
@@ -46,31 +46,62 @@ impl<'a> Checker<'a> {
     Ok(None)
   }
 
+  pub fn check_fn_expr(&mut self, fn_expr: &ast::FnExpr) -> CheckerResult<Type> {
+    let mut params_type = Vec::with_capacity(fn_expr.params.len());
+
+    for binding in &fn_expr.params {
+      let kind = self.check_binding(binding)?.unwrap();
+      params_type.push((binding.text(), kind));
+    }
+
+    let ret_type = match &fn_expr.ret_type {
+      Some(ret) => Some(Box::new(self.check_type(ret)?.unwrap())), // we can unwrap here because we
+      None => None,
+    };
+
+    let params = params_type.iter().map(|(_, ty)| ty.to_owned()).collect();
+
+    let fn_type = FnValue::new(params, ret_type.clone());
+
+    self.ctx.enter_scope();
+    // fn scope
+    for (name, ty) in params_type {
+      self.ctx.add_value(name.to_owned(), ty);
+    }
+    let body_type = self.check_stmt(&fn_expr.body)?;
+
+    let body_range = fn_expr.body.last_stmt_range().unwrap_or_else(|| fn_expr.body.range());
+
+    self.check_fn_ret_type(ret_type, body_type, body_range)?;
+
+    self.ctx.exit_scope();
+    Ok(Some(Type::Fn(fn_type)))
+  }
+
   pub fn check_fn_ret_type(
     &mut self,
     ret_type: Option<Box<Type>>,
     body_type: Option<Type>,
     range: Range,
   ) -> CheckerResult<()> {
+    // todo: we need to exit the scope here?
     match (ret_type, body_type) {
-      (Some(ret), Some(body)) => {
-        if !body.fits_in(&ret) {
-          let diag = TypeErr::Mismatched(&ret, &body, range);
-          self.ctx.exit_scope();
-          return Err(diag.into());
-        }
+      (Some(ret), Some(body)) if !body.fits_in(&ret) => {
+        let diag = TypeErr::Mismatched(&ret, &body, range);
+        self.ctx.exit_scope(); //  ensure that for every fn, we exit the scope
+        return Err(diag.into());
       }
       (Some(ret), None) => {
         let diag = TypeErr::ExpectedValue(&ret, range);
-        self.ctx.exit_scope();
+        self.ctx.exit_scope(); //  ensure that for every fn, we exit the scope
         return Err(diag.into());
       }
       (None, Some(body)) => {
         let diag = TypeErr::NoExpectedValue(&body, range);
-        self.ctx.exit_scope();
+        self.ctx.exit_scope(); //  ensure that for every fn, we exit the scope
         return Err(diag.into());
       }
-      (None, None) => {}
+      _ => {}
     }
     return Ok(None);
   }
