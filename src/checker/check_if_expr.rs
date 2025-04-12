19c23694c9118990038e5f998a0ea7e16bf2ca97
edilson258 <dev.258.edilson@gmail.com>
2025-04-12 08:55:52 +0200

    feat(ifexpr): check branck types

--- src/checker/check_if_expr.rs
+++ src/checker/check_if_expr.rs
@@ -7,14 +7,14 @@ impl Checker<'_> {
 	pub fn check_if_expr(&mut self, if_expr: &mut ast::IfExpr) -> MessageResult<TypedValue> {
 		let cond_type = self.check_expr(&mut if_expr.cond)?;
 		self.equal_type_expected(TypeId::BOOL, cond_type.type_id, if_expr.cond.get_range())?;
-		let then_type = self.check_expr(&mut if_expr.then)?;
-		let otherwise_type = self.check_expr(&mut if_expr.otherwise)?;
-		println!("{}", self.equal_type_id(then_type.type_id, otherwise_type.type_id));
-		self.equal_type_expected(
-			then_type.type_id,
-			otherwise_type.type_id,
-			if_expr.otherwise.get_range(),
-		)?;
-		Ok(TypedValue { type_id: then_type.type_id, ptr: 0 })
+		let then = self.check_expr(&mut if_expr.then)?;
+		let otherwise = self.check_expr(&mut if_expr.otherwise)?;
+		let if_expr_typed = match self.unify_types(then.type_id, otherwise.type_id)? {
+			Some(unified_type) => unified_type,
+			None => {
+				self.equal_type_expected(then.type_id, otherwise.type_id, if_expr.otherwise.get_range())?
+			}
+		};
+		Ok(TypedValue { type_id: if_expr_typed, ptr: 0 })
 	}
 }
